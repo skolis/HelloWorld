ae3e56873b98afd3e2ba5f4cf9484e3e950c0067
skolis <swetha.a0427@gmail.com>
2017-09-25 00:00:58 +0100

    added jenkins output

---
+++ jenkinsHello.java
@@ -0,0 +1,10 @@
+class JenkinsHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i=0; i<=3; i++){
+			System.out.println(" printing jenkins output" + i);
+		}
+	}
+
+}
